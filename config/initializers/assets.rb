6d801bd637c5c21bc7b891f62a80383a1ff2ce17
bman917 <bman917@gmail.com>
2015-06-28 23:45:51 +0800

    sales form

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( sales_new.js )
 Rails.application.config.assets.precompile += %w( item_index_dialog.js )
-Rails.application.config.assets.precompile += %w( item_layout.css )
+Rails.application.config.assets.precompile += %w( item_layout.css sales/all.css )
