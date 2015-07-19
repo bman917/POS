033cc7e41634a4dfee1f1c815314cf5bed25b662
jchan <bman917@gmail.com>
2015-07-19 23:51:28 +0800

    checkpoint

--- app/assets/javascripts/item_name_autocomplete_common.js
+++ app/assets/javascripts/item_name_autocomplete_common.js
@@ -1,9 +1,11 @@
 //Validates if the autocomplete form is properly pouplated
-function validate_item_autocomplete() {
-	// ret_val = true;
-	// if ($(item_id_css).val() == null) {
-	// 	ret_val = false;
-	// }
-	// return ret_val;
-	alert('hi');
+function validate_item_autocomplete(item_id_css,item_price_css) {
+	ret_val = true;
+	if ($(item_id_css).val().length    === 0 || 
+		$(item_price_css).val().length === 0) 
+	{
+		ret_val = false;
+	}
+	// console.log("validate_item_autocomplete = " + ret_val);
+	return ret_val;
 }
