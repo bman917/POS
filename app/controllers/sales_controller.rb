2c49ecd6173bb62327651f1e57ce712d83d6bc96
bman917 <bman917@gmail.com>
2015-07-31 02:05:20 +0800

    delete sales

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,12 +1,27 @@
 class SalesController < ApplicationController
-	def new_item
-	end
+  respond_to :html, :js
 
-	def new
-		@sale = Sale.find_by_id(session[:current_sale_id])
-		unless @sale
-			@sale = Sale.create
-			session[:current_sale_id] = @sale.id
-		end
-	end
+  def new_item
+  end
+
+  def new
+    @sale = Sale.find_by_id(session[:current_sale_id])
+    create_and_store_in_session unless @sale
+  end
+
+  def destroy
+    @sale = set_sale
+    @sale.destroy
+    create_and_store_in_session
+  end
+
+  private
+  def set_sale
+    @sale = Sale.find(params[:id])
+  end
+
+  def create_and_store_in_session
+    @sale = Sale.create
+    session[:current_sale_id] = @sale.id
+  end
 end
