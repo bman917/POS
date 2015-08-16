3c7afae4f18eecee7aeaf62cad67a73ca66f115f
bman917 <bman917@gmail.com>
2015-08-16 14:30:50 +0800

    sale date

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,4 @@ Rails.application.initialize!
 Time::DATE_FORMATS[:time] = "%B %d, %I:%M %p"
 Time::DATE_FORMATS[:med] = "%b %e, %Y"
 Time::DATE_FORMATS[:long] = "%B %e, %Y"
+Time::DATE_FORMATS[:sale_date] = "%b %d, %Y"
